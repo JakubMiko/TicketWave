530690ee228fd2351ad19bb7c7e301451e7f51bd
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-03-18 22:41:39 +0100

    Add translations, error handling and new session styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.human_attribute_name(attribute, options = {})
+    ''
+  end
 end
