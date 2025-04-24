8c98414f15bf2338dd596f63ddbaefaac63d28a2
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-04-24 09:29:18 +0200

    refactor ticket batches controller

---
+++ app/javascript/controllers/ticket_batches_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus";
+import consumer from "../channels/consumer";
+
+export default class extends Controller {
+  static targets = ["list"];
+  static values = { eventId: Number };
+
+  connect() {
+    this.subscription = consumer.subscriptions.create(
+      { channel: "TicketBatchesChannel", event_id: this.eventIdValue },
+      {
+        received: (data) => {
+          this.listTarget.innerHTML = data.html;
+        },
+      }
+    );
+  }
+
+  disconnect() {
+    if (this.subscription) {
+      consumer.subscriptions.remove(this.subscription);
+    }
+  }
+}
+ 
