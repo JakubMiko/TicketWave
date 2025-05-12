78077e14e58f763cacd2fc8b680835f7729373fa
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-05-12 23:13:00 +0200

    remove redundant method to call event from order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,12 +2,9 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :ticket_batch
   has_many :tickets, dependent: :destroy
+  has_one :event, through: :ticket_batch
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :total_price, presence: true, numericality: { greater_than: 0 }
   validates :status, presence: true
-
-  def event
-    ticket_batch.event
-  end
 end
